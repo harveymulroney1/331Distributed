cdfaa68f3b0661fa7b15c2cef0612ac5ade8c07f
harveymulroney1 <harveymulroney@gmail.com>
2025-11-16 21:53:40 +0000

    Backend ToDo Done

--- part1/src/main/java/server/AuctionImpl.java
+++ part1/src/main/java/server/AuctionImpl.java
@@ -19,7 +19,10 @@ public class AuctionImpl extends UnicastRemoteObject implements Auction {
         super();
         // TODO: initialise state variables
         users = new ConcurrentHashMap<Integer,String>();
+        itemID = new AtomicInteger();
+        userID = new AtomicInteger();
         items = new ConcurrentHashMap<Integer,AuctionItem>();
+        itemOwners = new ConcurrentHashMap<Integer,Integer>();
         closedItems = new ConcurrentHashMap<Integer,AuctionItem>();
 
     }
@@ -40,31 +43,48 @@ public class AuctionImpl extends UnicastRemoteObject implements Auction {
     public synchronized int newAuction(int userID, AuctionSaleItem item) {
         // TODO:
         // - If userID not registered, return -1 
+        if(!users.containsKey(userID)) { return -1;}
         // - Create a new itemID 
+        int ID = itemID.incrementAndGet();
         // - Store AuctionItem with initial highestBid = 0
+        AuctionItem a = new AuctionItem(ID,item.name,item.description,item.reservePrice);
+        items.put(ID,a);
         // - Record itemOwner[itemID] = userID
+        itemOwners.put(ID,userID);
         // - Return itemID
-        return -1; // TODO: replace with itemID or -1 on failure
+        return ID; 
     }
 
     @Override
     public synchronized AuctionItem getSpec(int itemID) {
         // TODO:
         // - Return the AuctionItem for itemID, or null if not found
-        return null;
+        
+        return items.get(itemID);
+        
     }
 
     @Override
     public synchronized AuctionItem[] listItems() {
         // TODO:
         // - Return all currently active items (do not return items from closed auctions).
-        return new AuctionItem[0];
+        return items.values().toArray(new AuctionItem[0]);
+        
     }
 
     @Override
     public synchronized boolean bid(int userID, int itemID, int price) {
         // TODO:
         // - If item missing OR user unknown OR item already closed -> return false.
+        if(!items.containsKey(itemID) || !users.containsKey(userID) || closedItems.containsKey(itemID))
+        {
+            AuctionItem i = items.get(itemID);
+            if(price>i.highestBid && price>=i.reservePrice){
+                i.highestBid = price;
+                i.highestBidder = userID;
+                return true;
+            }
+        }
         // - If price > current highestBid AND price >= reservePrice:
         //     - Update highestBid and return true.
         // - Otherwise, return false to indicate unsuccessful bid.
@@ -75,7 +95,15 @@ public class AuctionImpl extends UnicastRemoteObject implements Auction {
     public synchronized AuctionResult closeAuction(int userID, int itemID) {
         // TODO:
         // - Look up item; if missing, return null.
+        if(!items.containsKey(itemID)){
+            return null;
+        }
         // - Check owner: only creator can close; if not owner, return null.
+        AuctionItem i = items.get(itemID);
+        if(itemOwners.get(itemID)==userID)
+        {
+            return new AuctionResult(itemID, i.highestBidder, i.highestBid);
+        }
         // - Mark item as closed (add to closedItems) and remove from active items map.
         // - Return AuctionResult(itemID, winningUser=userID, price=highestBid).
         return null;
--- part1/src/main/java/server/AuctionItem.java
+++ part1/src/main/java/server/AuctionItem.java
@@ -7,9 +7,10 @@ public class AuctionItem implements Serializable {
     public final String description;
     public final int reservePrice;
     public int highestBid;
+    public int highestBidder;
 
     public AuctionItem(int id, String n, String d, int r) {
         this.itemID = id; this.name = n; this.description = d;
-        this.reservePrice = r; this.highestBid = 0;
+        this.reservePrice = r; this.highestBid = 0; this.highestBidder =0;
     }
 }
